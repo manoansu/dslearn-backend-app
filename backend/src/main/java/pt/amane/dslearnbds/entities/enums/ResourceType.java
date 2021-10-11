7003972524e139ffd9a1860c83c7d2cfc12717e7
Ansumane Mané <manoansu@gmail.com>
2021-10-11 20:14:39 +0100

    resource clas is implemented

---
+++ backend/src/main/java/pt/amane/dslearnbds/entities/enums/ResourceType.java
@@ -0,0 +1,8 @@
+package pt.amane.dslearnbds.entities.enums;
+
+public enum ResourceType {
+	LESSON_ONLY,
+	LESSON_TASK,
+	FORUM,
+	EXTERNAL_LINK;
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('BootCamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1); 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML','Trilha Principal',1,'https://www.publicdomainpictures.net/pictures/390000/nahled/course-introduction.jpg',1,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum','Tira suas duidas',2,'https://www.publicdomainpictures.net/pictures/390000/nahled/course-introduction.jpg',2,1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives','Lives exclusiva para turma',3,'https://www.publicdomainpictures.net/pictures/390000/nahled/course-introduction.jpg',0,1);
